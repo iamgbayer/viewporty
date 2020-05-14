23eb3f6f477182d72aed65ac61b990697fdfdc23
unknown <guiilherme.bayer@gmail.com>
2020-05-13 23:19:17 -0300

    Added start and dev commands separated

--- packages/landing/next.config.js
+++ packages/landing/next.config.js
@@ -8,7 +8,6 @@ const PATH = require('./path')
 module.exports = withPlugins(
   [withFonts, withImages, withTM(['@responsivy/components'])],
   {
-    target: 'serverless',
     webpack: (config) => {
       config.resolve.alias['@'] = PATH.source
 
